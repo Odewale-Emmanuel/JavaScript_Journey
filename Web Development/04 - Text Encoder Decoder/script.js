70755841955652468428383c783d0b00aef55345
Odewale-Emmanuel <odebzemmygee@gmail.com>
2024-04-17 18:40:12 +0100

    update

--- Web Development/04 - Text Encoder Decoder/script.js
+++ Web Development/04 - Text Encoder Decoder/script.js
@@ -1,28 +1,50 @@
-const shift = document.getElementById('shift')
-const text = document.getElementById('text-field')
-const output = document.querySelector('.output')
+const shift = document.getElementById('shift');
+const text = document.getElementById('text-field');
+const output = document.querySelector('.output');
+const copyToClipboard = document.querySelector('i.copy-to-clipboard');
+const infoText = document.querySelector('p.info-text');
+
 
 function encodeDecode() {
     const shiftValue = Number(shift.value)
-    const input = text.value
+    const input = text.value;
     if (shiftValue >= -25 && shiftValue <= 25) {
         if (input) {
-            const result = []
+            const result = [];
 
             for (let element of input) {
-                const elementCode = element.charCodeAt()
-                result.push(String.fromCharCode(elementCode + shiftValue))
+                const elementCode = element.charCodeAt();
+                result.push(String.fromCharCode(elementCode + shiftValue));
             }
-            output.innerHTML = result.join('')
+            output.innerHTML = result.join('');
         } else {
-            output.innerHTML = 'output goes here'
+            output.innerHTML = 'output goes here';
         }
     }
     else {
-        output.innerHTML = 'enter a valid shift range between -25 and +25'
+        output.innerHTML = 'enter a valid shift range between -25 and +25';
     }
 
 }
+function copy() {
+    navigator.clipboard.writeText(output.innerHTML)
+        .then(() => {
+            infoText.innerHTML = 'Text copied successfully to clipboard';
+            infoText.classList.add('rounded', 'py-1', 'px-2', 'my-2', 'w-100', 'text-white', 'bg-success');
+            infoText.style.display = 'block';
+        })
+        .catch(error => {
+            infoText.innerHTML = 'An error occurred while trying to copy text to clipboard';
+            infoText.classList.add('rounded', 'py-1', 'px-2', 'my-2', 'w-100', 'text-white', 'bg-danger');
+            infoText.style.display = 'block';
+        });
 
-text.addEventListener('input', encodeDecode)
-shift.addEventListener('change', encodeDecode)
+    setTimeout(() => {
+        infoText.style.display = 'none';
+    }, 3500);
+}
+
+
+text.addEventListener('input', encodeDecode);
+shift.addEventListener('change', encodeDecode);
+copyToClipboard.addEventListener('click', copy);
