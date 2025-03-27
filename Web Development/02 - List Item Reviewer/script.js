9912feff9ffb9b81047e3df2721e22b86f94c429
odewale emmanuel <odebzemmygee@gmail.com>
2025-03-27 23:25:52 +0100

    items highlight feature added and previous button bug  fix

--- Web Development/02 - List Item Reviewer/script.js
+++ Web Development/02 - List Item Reviewer/script.js
@@ -1,36 +1,60 @@
-let next, previous;
-next = 0;
+let activeIndex = 0, handleButtonGrowth;
 const itemNumber = document.querySelector("span.item-number");
 const reviewedItem = document.querySelector("p.reviewed-item");
 const btnPlus = document.querySelector("button.btn-plus");
 const btnMinus = document.querySelector("button.btn-minus");
 const list = document.querySelector("ul.list-container").children;
 
-itemNumber.innerHTML = `Item ${next + 1}`;
-reviewedItem.innerHTML = list[next].innerHTML;
+itemNumber.innerHTML = `Item ${activeIndex + 1}`;
+reviewedItem.innerHTML = list[activeIndex].innerHTML;
+highlight(activeIndex);
+btnPlus.style.transition = "all .2s ease-in-out";
+btnMinus.style.transition = "all .2s ease-in-out";
+
+
+function highlight(itemIndex) {
+    const listItems = [...list];
+    listItems.map((item, index) => {
+        if (itemIndex === index) {
+            item.classList.toggle("text-danger");
+        } else {
+            item.classList.remove("text-danger");
+        }
+    })
+}
+
 
 btnPlus.addEventListener('click', (event) => {
-    if (next < (list.length - 2)) {
-        next++;
-        previous = next - 1;
-        itemNumber.innerHTML = `Item ${next + 1}`;
-        reviewedItem.innerHTML = list[next].innerHTML;
+    if (activeIndex < (list.length - 2)) {
+        activeIndex++;
+        itemNumber.innerHTML = `Item ${activeIndex + 1}`;
+        reviewedItem.innerHTML = list[activeIndex].innerHTML;
     }
     else {
-        next = list.length - 1;
+        activeIndex = list.length - 1;
         itemNumber.innerHTML = `Item ${list.length} is the last item on the list`;
-        reviewedItem.innerHTML = list[next].innerHTML;
-        previous = next - 1;
+        reviewedItem.innerHTML = list[activeIndex].innerHTML;
     }
+
+    highlight(activeIndex);
+    clearTimeout(handleButtonGrowth);
+    event.target.style.transform = "scale(1.2)";
+    handleButtonGrowth = setTimeout(() => {
+        event.target.style.transform = "scale(1)";
+    }, 200)
 });
 
 btnMinus.addEventListener('click', (event) => {
-    if (previous >= 0) {
-        next = previous + 1;
-        itemNumber.innerHTML = `Item ${previous + 1}`;
-        reviewedItem.innerHTML = list[previous].innerHTML;
-        previous--;
+    if (activeIndex > 0) {
+        activeIndex--;
+        itemNumber.innerHTML = `Item ${activeIndex + 1}`;
+        reviewedItem.innerHTML = list[activeIndex].innerHTML;
     }
-
+    highlight(activeIndex);
+    clearTimeout(handleButtonGrowth);
+    event.target.style.transform = "scale(1.2)";
+    handleButtonGrowth = setTimeout(() => {
+        event.target.style.transform = "scale(1)";
+    }, 200)
 });
 
